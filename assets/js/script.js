0cbeb22c42049c282547a17205f4988f9068b436
NAQEEB UL REHMAN <130925302+NAQEEB26@users.noreply.github.com>
2024-07-07 23:36:02 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["An Enterpreneur", "Web Developer", "Tech Enthusiast" ,"WordPress Developer", ""],
+    strings: ["Tech Enthusiast", "WordPress Developer", "Web Developer", "Young Enterpreneur",  " " ],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
